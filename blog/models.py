2e86cb22410dff2a7aca124941ab40aafab3c5c2
Ratish149 <ratish.shakya149@gmail.com>
2025-04-10 16:13:43 +0545

    dfd

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,14 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from tinymce import models as tinymce_models
 from activity.models import Activity
 
+
 class Author(models.Model):
-    name = models.CharField(max_length=200) 
+    name = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     picture = models.FileField()
+    picture_alt_description = models.CharField(
+        max_length=300, blank=True, null=True)
     about = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -17,12 +20,14 @@ class Author(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
 class Category(models.Model):
     category_name = models.CharField(max_length=200, primary_key=True)
     category_image = models.FileField(blank=True)
 
     def __str__(self):
         return self.category_name
+
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=200)
@@ -36,14 +41,15 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.CharField(max_length=300)
     title = models.CharField(max_length=500)
-    blog_duration_to_read = models.CharField(max_length=100,blank=True)
+    blog_duration_to_read = models.CharField(max_length=100, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     thumbnail_image = models.FileField()
     thumbnail_image_alt_description = models.CharField(max_length=300)
     blog_content = tinymce_models.HTMLField(blank=True)
     tags = models.ManyToManyField(Tag)
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
-    related_activities = models.ManyToManyField('activity.Activity', blank=True, related_name='posts')
+    related_activities = models.ManyToManyField(
+        'activity.Activity', blank=True, related_name='posts')
     meta_title = models.CharField(max_length=200)
     meta_description = models.TextField()
 
@@ -52,4 +58,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
--- blog/serializers.py
+++ blog/serializers.py
@@ -6,6 +6,7 @@ from django.utils.html import strip_tags
 from django.utils.safestring import mark_safe
 from bs4 import BeautifulSoup
 
+
 class HTMLField(serializers.CharField):
     def to_representation(self, value):
         return str(BeautifulSoup(value, 'html.parser'))
@@ -13,16 +14,19 @@ class HTMLField(serializers.CharField):
     def to_internal_value(self, data):
         return data
 
+
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
         depth = 2
 
+
 class CategorySmallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('category_name',)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -30,10 +34,12 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 2
 
+
 class TagSmallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('tag_name',)
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -51,7 +57,7 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 2
         ordering = ['-created_at']
-    
+
     def get_blog_content(self, obj):
         html_string = obj.blog_content
         soup = BeautifulSoup(html_string, 'html.parser')
@@ -61,29 +67,35 @@ class PostSerializer(serializers.ModelSerializer):
         updated_html_string = str(soup)
         return mark_safe(updated_html_string)
 
+
 class PostSmallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        exclude = ['blog_content','related_activities']
+        exclude = ['blog_content', 'related_activities']
         depth = 1
         ordering = ['-created_at']
+
 
 class NavbarPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields=('id','title','slug')
+        fields = ('id', 'title', 'slug')
+
 
 class AuthorSmallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('id','name','picture')
+        fields = ('id', 'name', 'picture', 'picture_alt_description')
+
 
 class LandingPagePostSerializer(serializers.ModelSerializer):
     author = AuthorSmallSerializer()
 
     class Meta:
         model = Post
-        fields=('id','thumbnail_image','updated_at','created_at','blog_duration_to_read','slug','title','thumbnail_image_alt_description','author','meta_description')
+        fields = ('id', 'thumbnail_image', 'updated_at', 'created_at', 'blog_duration_to_read',
+                  'slug', 'title', 'thumbnail_image_alt_description', 'author', 'meta_description')
+
 
 class PostSlugSerializer(serializers.ModelSerializer):
     class Meta:
