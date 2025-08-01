563d98de0b89b7494bedb7f67be88bf0f298dc8a
Ratish149 <ratish.shakya149@gmail.com>
2025-08-01 17:26:50 +0545

    dfd

--- activity/views.py
+++ activity/views.py
@@ -160,7 +160,7 @@ def activities_collection(request):
 @api_view(['GET'])
 def activities_slug(request):
     if request.method == 'GET':
-        activities = Activity.objects.only('slug')
+        activities = Activity.objects.only('id', 'slug')
         serializer_activities = ActivitySlugSerializer(activities, many=True)
         return Response(serializer_activities.data)
 
